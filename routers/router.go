5d5f77b38d627115812684fa484b38c36bf79279
Mahdi Bolow <boolow5@gmail.com>
2017-04-04 21:59:23 +0300

    added auth routes

--- routers/router.go
+++ routers/router.go
@@ -7,5 +7,8 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
+	beego.Router("/login", &controllers.AuthController{}, "get:Get;post:Post")
+	beego.Router("/logout", &controllers.AuthController{}, "*:Logout")
+	beego.Router("/register", &controllers.AuthController{}, "get:GetRegister;post:PostRegister")
 	beego.Router("/translate/:word/:args", &controllers.MiscAPIController{}, "*:GetTranslation")
 }
