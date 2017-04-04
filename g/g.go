6f9f84771b726602c2e064a8ac72a2c7985dd442
Mahdi Bolow <boolow5@gmail.com>
2017-04-04 21:51:34 +0300

    added app_id to access BolAuth

--- g/g.go
+++ g/g.go
@@ -2,12 +2,19 @@ package g
 
 import (
 	"log"
+	"os"
 
 	"github.com/astaxie/beego/session"
 	_ "github.com/astaxie/beego/session/redis"
 )
 
+var APP_ID string
+
 func InitEnv() {
+	APP_ID = os.Getenv("APP_ID")
+	if APP_ID == "" {
+		APP_ID = "1234567890"
+	}
 	globalSessions, err := session.NewManager("redis", &session.ManagerConfig{CookieName: "gosessionid", Gclifetime: 3600, ProviderConfig: "127.0.0.1:6379"})
 	if err != nil {
 		log.Fatalln(err)
